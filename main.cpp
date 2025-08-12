96b514d77a13ce6be329befd8ff7b642cb578106
shyamdotexe <shyamshivajij07@gmail.com>
2025-08-12 22:42:10 +0530

    Banking Part Pending

--- main.cpp
+++ main.cpp
@@ -7,7 +7,25 @@
 #include <bits/stdc++.h>
 #include <conio.h>
 #include <fstream>
+#include <openssl/sha.h>
+#include <sstream>
 using namespace std;
+
+string hashing_pin(string pin)
+{   
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX ctx;
+    SHA256_Init(&ctx);
+    SHA256_Update(&ctx, pin.c_str(), pin.size());
+    SHA256_Final(hash, &ctx);
+    stringstream ss;
+    for(auto i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        ss << hex << setw(2) << setfill('0') << (int)hash[i];
+    }
+
+    return ss.str();
+}
 
 class BankAccount 
 {
@@ -15,8 +33,7 @@ class BankAccount
         string accountHolder;
         int accountNumber;
         double balance;
-        string pin;
-        static int accountCount;
+        string pass;
     
     protected:
         double B;
@@ -25,27 +42,61 @@ class BankAccount
 
     
     public:
+        static int accountCount;
         BankAccount(string name, double initialBalance, string pin)
             {
                 accountHolder = name;
                 accountNumber = accountCount;
-                this -> pin = pin ;
+                pass = hashing_pin(pin) ;
                 balance = initialBalance;
                 accountCount++;
-                ofstream file("Account_Details.txt", ios::app) ;
-                file << accountHolder << "\t|\t"<< accountNumber <<"\t|\t" << balance << endl;
+                ofstream file("Account_Details.csv", ios::app) ;
+                file << accountHolder<<','<< accountNumber<<','  << pass<<',' << balance << endl;
                 file.close() ;
+                cout << "Account Created Successfully !" << endl << "Your Account Number is : " << accountNumber;
             }
         virtual void deposit(double amount) = 0;
         virtual void withdraw(double amount) = 0;
  
-    void displayAccDetails()
-  {
-    cout<<"Account Number : "<< accountNumber<< endl <<"Account Holder Name : "<<accountHolder<<endl<<"Balance : $"<<fixed<<setprecision(4)<<balance;
+    void displayAccDetails(int accNo, string pass)
+  { 
+    ifstream inputFile("Account_Details.csv");
+    string entered_pin = hashing_pin(pass);
+    bool result = false;
+    string line, name, pin, accountNoStr, fileBalance;
+    int accountNo;
+   while(getline(inputFile, line))
+{   
+    stringstream ss(line);
+    if(! getline(ss, name, ',')) continue;
+    if(! getline(ss, accountNoStr, ',')) continue;
+    if(! getline(ss, pin, ',')) continue;
+    if(! getline(ss, fileBalance, ',')) continue;
+
+    accountNo = stoi(accountNoStr);
+
+   
+
+    if(accountNo == accNo && entered_pin == pin)
+    {   
+        cout <<endl<<"Account Holder Name: "<<name << endl;
+        cout <<"Account Number: "<< accountNo << endl;
+        cout <<"Balance : $"<< fileBalance << endl;
+        result = true;
+        break;
+    } 
+
+ 
+    }  
+    if(!result)
+    {
+        cout<< "Account Not Found !! Invalid Account No or PIN"<<endl;
+    }
+    inputFile.close();
   }
 };
 
-int BankAccount :: accountCount = 1;
+int BankAccount :: accountCount = 1000;
 
 class Banking : public BankAccount
 {   
@@ -73,11 +124,11 @@ class Banking : public BankAccount
         }
 };
 
-string pinCreation(int length = 6)
+string pin_updation(int length = 6)
 {
     string pin = "";
     char ch;
-    cout<<"Enter a 6 Digit Pin :"<<endl;
+    cout<<"Enter 6 Digit Pin :"<<endl;
     while(pin.length() < length)
     {
         ch = _getch();
@@ -101,6 +152,7 @@ int main()
   double bal;
   string pass;
   double amount;
+  int accNo;
   cin>>choice;
   cin.ignore();
   
@@ -111,12 +163,12 @@ int main()
     cout<<"Enter your Name : " ;
     getline(cin,name);
     cout<<"\nCreate PIN for your Account\n";
-    pass = pinCreation();
+    pass = pin_updation();
     cout<<"\nEnter the Initial Deposit amount : ";
     cin>>bal;
     Banking b1(name, bal, pass);
     p = &b1;
-    p -> displayAccDetails();
+    
   }
   else if(choice == 2)
   {
@@ -125,7 +177,7 @@ int main()
     int a;
     cin>>a;
     if(a == 1)
-    {
+    {   
         cout<<"Enter the Amount to be Withdrawn : $";
         cin>>amount;
         p -> withdraw(amount);
@@ -142,8 +194,12 @@ int main()
     }
   }
   else if(choice == 3)
-  {
-    cout << p->getBalance() << endl;
+  { 
+    cout << " Enter Account Number: ";
+    cin>>accNo;
+    cin.ignore();
+    pass = pin_updation();
+    p -> displayAccDetails(accNo, pass);
   }
   return 0;
 
